2c7531f2b7e6282281cde5d659dfc6cfd1d40c28
Stefan von Stein <stefanvstein@gmail.com>
2024-03-10 11:24:02 +0100

    run namespace rather than script, and run in-in-ns

--- src/repl_test.clj
+++ src/repl_test.clj
@@ -4,7 +4,8 @@
    [clojure.core.server :as server]
    [clojure.java.io :as io]
    [clojure.main :as main]
-   [clojure.pprint :as pp]))
+   [clojure.pprint :as pp]
+   [clojure.string :as str]))
 
 (defn write [print-writers txt]
   (doseq [w print-writers]
@@ -90,7 +91,7 @@
 
 (defn translate-ns
   "Translates ns instructions like the ns macro, but renames the ns by adding a postfix name. Does not affect *loaded-libs* , doc or any other namespace meta data, nor touch thread binding. nil on any other form"
-  [form {:keys [use-target]}]
+  [form {:keys [use-target?]}]
   (when (ns? form)
     (let [[_ the-ns & args] form
           params-without-doc (if (string? (first args))
@@ -103,16 +104,22 @@
                        [(list 'clojure.core/use ''clojure.core)])
           ns-def [(list 'clojure.core/in-ns
                         (list 'quote (unique-ns the-ns)))]
-          use-def (when use-target 
+          use-def (when use-target? 
                     [(list 'clojure.core/use (list 'quote the-ns))])
-
-          ;;; What should we do with private vars? Especially private values?. Should we move them to current ns, or refer them in place. Why do we have a namespace? Should we load everything in current namespace?
+;;; What should we do with private vars? Especially private values?. Should we move them to current ns, or refer them in place. Why do we have a namespace? Should we load everything in current namespace?
           #_#_privates (refer-privates the-ns)]
       (concat ns-def
               clojure-ns
               refers
               use-def
+
               #_privates))))
+
+(defn only-in-ns [form]
+  (when (ns? form)
+    (let [[_ the-ns] form]
+      [(list 'clojure.core/in-ns
+             (list 'quote the-ns))])))
 
 (defn test-form [form {:keys [active-comment]}]
   (when (and (list? form)
@@ -141,11 +148,36 @@
      test-input
      any-input)))
 
+(defn only-test [input config]
+  (or (only-in-ns input)
+      (test-form input config) []))
 
+(def ^:private request-prompt (Object.))
+(def ^:private request-exit (Object.))
+
+(defn read-eval-print [{:keys [input-selector]
+                        :as config}]
+  (let [read-eval *read-eval*
+        input (main/with-read-known (server/repl-read request-prompt request-exit))]
+    (if (#{request-prompt request-exit} input)
+      input
+      (let [inputs (input-selector input config)]
+        (loop [input (first inputs) inputs (next inputs)]
+          
+          (when input
+            (pp/pprint input)
+            (let [value (binding [*read-eval* read-eval] (eval input))]
+              (set! *3 *2)
+              (set! *2 *1)
+              (set! *1 value)
+              (print "=> ")
+              (pp/pprint value)
+              (println))
+            (when inputs (recur (first inputs) (next inputs)))))))))
 
 (defn repl
   "Transcript-making REPL. Like a normal REPL except:
-
+  
 - pretty prints inputs
 - prints '=> ' before pretty printing results
 - throws on exception
@@ -153,63 +185,72 @@
 Not intended for interactive use -- point this at a file to
 produce a transcript as-if a human had performed the
 interactions."
-  [{:keys [input-selector]
-    :as config}]
+  [{:keys [new-classpath?] :as config}]
   (let [cl (.getContextClassLoader (Thread/currentThread))]
-    (.setContextClassLoader (Thread/currentThread) (clojure.lang.DynamicClassLoader. cl)))
-  (let [#_#_test-var (var test-comment)
-        request-prompt (Object.)
-        request-exit (Object.)
-        read-eval-print (fn []
-                          (let [read-eval *read-eval*
-                                input (main/with-read-known (server/repl-read request-prompt request-exit))]
-                            (if (#{request-prompt request-exit} input)
-                              input
-                              (let [inputs (input-selector input config)]
-                                (loop [input (first inputs) inputs (next inputs)]
+    (try
+      (when new-classpath?
+        (.setContextClassLoader (Thread/currentThread)
+                                (clojure.lang.DynamicClassLoader. cl)))
+      (main/with-bindings
+        (binding [*exit-items* (atom ())]
+          (try
+            (loop []
+              (let [value (read-eval-print config)]
+                (when-not (identical? value request-exit)
+                  (recur))))
+            (finally
+              (doseq [item @*exit-items*]
+                (item))))))
+      (finally
+        (when new-classpath?
+          (.setContextClassLoader (Thread/currentThread) cl))))))
 
-                                  (when input
-                                    (pp/pprint input)
-                                    (let [value (binding [*read-eval* read-eval] (eval input))]
-                                      (set! *3 *2)
-                                      (set! *2 *1)
-                                      (set! *1 value)
-                                      (print "=> ")
-                                      (pp/pprint value)
-                                      (println))
-                                    (when inputs (recur (first inputs) (next inputs)))))))))]
-    (main/with-bindings
-      (binding [*exit-items* (atom ())]
-        (try
-          (loop []
-            (let [value (read-eval-print)]
-              (when-not (identical? value request-exit)
-                (recur))))
-          (finally
-            (doseq [item @*exit-items*]
-              (item))))))))
+(defn as-path [s]
+  (str (.. s
+           (replace \- \_)
+           (replace \. \/))))
 
 (defn- repl-on
-  [r config]
-  (binding [*ns* *ns*]
-    (with-open [rdr (LineNumberingPushbackReader. (io/reader r))]
-      (binding [*source-path* (str r) *in* rdr]
-        (repl config)))))
+  [ns config]
+  (let [ns-str (name ns)
+        path (as-path ns-str)
+        cl (clojure.lang.RT/baseLoader)]
+    (binding [*ns* *ns*]
+      (with-open [rdr (-> (or (.getResourceAsStream cl (str path ".clj"))
+                              (.getResourceAsStream cl (str path ".cljc"))
+                              (throw (ex-info (str "Can't source for " ns-str)                                                      {:looking-for (str path ".clj")})))
+                          io/reader
+                          LineNumberingPushbackReader.)]
+        (binding [*source-path* (str ns-str) *in* rdr]
+          (repl config))))))
 
 (defn run-as-use
-  "Run script through transcripting repl in a tearoff namespace."
-  [script]
-  (repl-on script {:input-selector require-and-use
-                   :use-target true
-                   :active-comment (var test-comment)}))
+  "Run content in test-comment of ns in a tearoff namespace where ns is refered."
+  [ns]
+  (repl-on ns {:input-selector require-and-use
+               :use-target? true
+               :new-classpath? true
+               :active-comment (var test-comment)}))
 
 (defn run-eval-all
-  "Run script through transcripting repl in a tearoff namespace."
-  [script]
-  (repl-on script {:input-selector eval-all
-                   :active-comment (var test-comment)}))
+  "Run content in test-comment of ns in a tearoff namespace in which all of ns is evaluated."
+  
+  [ns]
+  (repl-on ns {:input-selector eval-all
+               :new-classpath? true
+               :active-comment (var test-comment)}))
 
+(defn run-in-ns
+  "Run content in test-comment of ns in it's own namespace"
+  [ns]
+  (repl-on ns {:input-selector only-test
+               :active-comment (var test-comment)}))
 (comment
-  (run-as-use "src/tryout.clj")
-  (run-eval-all "src/tryout.clj")
+  (run-as-use 'tryout)
+  (run-eval-all "tryout")
+  (run-in-ns 'tryout)
+  
   #_no )
+
+
+
