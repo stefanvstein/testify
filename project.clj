9c3e468396c69972e0f4bcd3b9a953abc52dea9b
Stefan von Stein <stefanvstein@gmail.com>
2024-04-06 21:28:27 +0200

    v030

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vstein/testify "0.2.0"
+(defproject org.clojars.vstein/testify "0.3.0"
   :description "Your comment became a usable snippet of code! Testify turns these comments into automatically evaluated scripts, while still remaining embedded as a comment within your code. You rename the comment to test-comment and evaluate (eval-in-ns 'your-namespace) to automate evaluation."
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :url "https://github.com/stefanvstein/testify"
